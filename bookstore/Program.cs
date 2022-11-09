e4f521ff44990262bfea94c252e8916c130540a2
novelkhan <novel4004@gmail.com>
2022-11-09 19:46:59 +0600

    (#22) Razor file compilation in asp.net core

--- bookstore/Program.cs
+++ bookstore/Program.cs
@@ -1,18 +1,23 @@
 using bookstore.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using System.Collections.Generic;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+#if DEBUG
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();///(#22) Razor file compilation in asp.net core// For Runtime Compilation
+#endif
 
 var app = builder.Build();
 
@@ -30,6 +35,12 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+//app.UseStaticFiles(new StaticFileOptions()  //(#20) Static files (Img, css & js) in asp.net core                  // Static Files From Other Location 
+//{
+//    //FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory() + "MyStaticFiles")
+//    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "MyStaticFiles")),
+//    RequestPath = "/MyStaticFiles"
+//});
 
 app.UseRouting();
 
